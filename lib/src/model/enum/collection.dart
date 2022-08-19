6230b7b94f28b49d008059793b490e0c49164467
e.chandara <eamchandara@labs.haystack.asia>
2022-08-19 15:28:37 +0700

    [UPDATE]: add total expenses detail

--- lib/src/model/enum/collection.dart
+++ lib/src/model/enum/collection.dart
@@ -1,8 +1,8 @@
 enum Collection {
-  // purchaseItems('purchase_items'),
-  purchaseItems('purchase_items_debug'),
-  // totalExpenses('total_expenses'),
-  totalExpenses('total_expenses_debug');
+  purchaseItems('purchase_items'),
+  // purchaseItems('purchase_items_debug'),
+  totalExpenses('total_expenses');
+  // totalExpenses('total_expenses_debug');
 
   const Collection(this.value);
 
