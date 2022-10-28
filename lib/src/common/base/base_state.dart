8d6c338a09343d2b61173fcbfe4e9fac933a3a74
e.chandara <eamchandara@labs.haystack.asia>
2022-10-28 14:59:25 +0700

    reduce awaitSetState duration

--- lib/src/common/base/base_state.dart
+++ lib/src/common/base/base_state.dart
@@ -6,8 +6,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   @protected
   final FirestoreService firestoreService = FirestoreService();
 
-  Future<void> awaitSetState() async =>
-      Future<void>.delayed(const Duration(seconds: 1), () {
-        setState(() {});
-      });
+  Future<void> awaitSetState() async => Future<void>.delayed(
+      const Duration(milliseconds: 100), () => setState(() {}));
 }
